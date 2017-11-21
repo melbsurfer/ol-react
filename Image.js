4e2e12b2a6d84ddc2627257d6a23cf259175e22d
melbsurfer <melbsurfer@gmail.com>
2017-11-21 10:45:27 -0500

    Added image thumbnail components.

--- Image.js
+++ Image.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import { THUMBNAIL_SERVICE } from './config'
+import ImageThumbnail from './ImageThumbnail';
+
 class Image extends Component {
   render() {
+    console.log(this.props);
     const divStyle = {
       borderColor: 'red',
       border: 'solid',
@@ -14,9 +18,13 @@ class Image extends Component {
     }
     return (
       <div style={divStyle}>
-        <p style={pStyle}>
-          Image
-        </p>
+        <p style={pStyle}>Image: {this.props.data.properties.id}</p>
+        <ImageThumbnail
+          filename={this.props.data.properties.filename}
+          entry_id={this.props.data.properties.entry_id}
+          size={THUMBNAIL_SERVICE.size}
+          format={THUMBNAIL_SERVICE.format}
+        />
       </div>
     )
   }
--- ImageGrid.js
+++ ImageGrid.js
@@ -1,28 +1,60 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import Wfs from './libs/Wfs';
 import Image from './Image';
 
 class ImageGrid extends Component {
-
-    render() {
-
-        const divStyle = {
-          borderColor: '#ccc',
-          border: 'solid'
-        };
-
-        const pStyle = {
-          color: 'purple'
-        }
-
-        return (
-          <div style={divStyle}>
-            <p style={pStyle}>Image Grid</p>
-            <Image />
-          </div>
-        )
+  constructor(props){
+    super(props)
+    this.state = {
+      images: [],
+      totalFeatures: 'Calculating...',
     }
+  }
+
+  componentDidMount(){
+
+    const wfs = new Wfs();
+
+    wfs.getDataHits().then(function(hits) {
+      //console.log('wfs.getDataHits: ', hits);
+      this.setState({totalFeatures: hits});
+    }.bind(this));
+
+    wfs.getData().then(function(data) {
+      //console.log('wfs.getData: ', data);
+      this.setState({images: data});
+    }.bind(this));
+  }
+
+  render() {
+
+    console.log('state (in render): ', this.state);
+
+    const divStyle = {
+      borderColor: '#ccc',
+      border: 'solid'
+    };
+
+    const pStyle = {
+      color: 'purple'
+    }
+    if (this.state.images.length === 0) {
+      return <p>Loading...</p>
+    }
+    else {
+      return (
+        <div style={divStyle}>
+          <p style={pStyle}>Image Grid</p>
+          {this.state.images.map(function(image, i) {
+            return <Image key={i} data={image} />
+          })}
+        </div>
+      )
+    }
+
+  }
 
 }
 

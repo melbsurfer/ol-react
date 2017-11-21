ff5e1ad8eb457f9af3c928d5cbf4581f61e0a61f
melbsurfer <melbsurfer@gmail.com>
2017-11-21 10:47:42 -0500

    Forgot to add thumb .js file to the commit.

---
+++ ImageThumbnail.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { THUMBNAIL_SERVICE } from './config';
+
+class ImageThumbnail extends Component {
+
+  constructor(props) {
+
+    super(props);
+
+    this.state = {
+      thumbUrl: ''
+    }
+
+  }
+
+  componentDidMount() {
+    console.log('props: ', this.props);
+    let url = THUMBNAIL_SERVICE.url +
+      '?filename=' + this.props.filename +
+      '&entry_id=' + this.props.entry_id +
+      '&size=' + this.props.size +
+      '&format=' + this.props.format
+
+      this.setState({
+        thumbUrl: url
+      });
+
+  }
+
+  render() {
+    return (
+      <div>
+        <img src={this.state.thumbUrl} />
+      </div>
+
+    )
+  }
+}
+
+// ImageThumbnail.propTypes = {
+//   filename: React.PropTypes.string.isRequired,
+//   entry_id: React.PropTypes.string.isRequired,
+//   size: React.PropTypes.number.isRequired,
+//   format: React.PropTypes.string.isRequired
+// }
+
+export default ImageThumbnail;
